5fa25fef7a6968689d0b9975de9181e641aff6c0
sj900211 <sj900211@gmail.com>
2020-05-07 13:49:23 +0900

    #try embed youtube

--- assets/js/plugins/jquery.fitvids.js
+++ assets/js/plugins/jquery.fitvids.js
@@ -34,7 +34,7 @@
     return this.each(function(){
       var selectors = [
         'iframe[src*="player.vimeo.com"]',
-        'iframe[src*="youtube.com"]',
+        //'iframe[src*="youtube.com"]',
         'iframe[src*="youtube-nocookie.com"]',
         'iframe[src*="kickstarter.com"][src*="video.html"]',
         'object',
